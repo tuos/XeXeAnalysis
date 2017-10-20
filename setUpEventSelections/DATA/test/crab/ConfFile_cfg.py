0c1bdf596f683cb27e86f410f5698324a44248d3
Shengquan <shengquan.tuo@cern.ch>
2017-10-20 16:44:22 -0500

    add crab cfg running with all MB PDs

---
+++ setUpEventSelections/DATA/test/crab/ConfFile_cfg.py
@@ -0,0 +1,111 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+#process.MessageLogger.cerr.FwkReport.reportEvery = 100
+#process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+'root://cmsxrootd.fnal.gov//store/hidata/XeXeRun2017/HIMinimumBias1/AOD/PromptReco-v1/000/304/906/00000/CEF3F710-BDB1-E711-9652-02163E019BB5.root'
+    )
+)
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '92X_dataRun2_Prompt_Candidate_forXeXe_v1', '')
+
+#MB triggers
+process.load("HLTrigger.HLTfilters.hltHighLevel_cfi")
+process.hltHIL1MinimumBiasHF_OR_SinglePixelTrack = process.hltHighLevel.clone()
+process.hltHIL1MinimumBiasHF_OR_SinglePixelTrack.HLTPaths = ["HLT_HIL1MinimumBiasHF_OR_SinglePixelTrack_part*"]
+process.hltHIL1MinimumBiasHF_OR_SinglePixelTrack.andOr = cms.bool(True)
+process.hltHIL1MinimumBiasHF_OR_SinglePixelTrack.throw = cms.bool(False)
+
+#Event Selections:
+#process.load('HeavyIonsAnalysis.Configuration.collisionEventSelection_cff')
+#primaryVertexFilter
+process.primaryVertexFilter = cms.EDFilter("VertexSelector",
+    src = cms.InputTag("offlinePrimaryVertices"),
+    cut = cms.string("!isFake && abs(z) <= 25 && position.Rho <= 2 && tracksSize >= 2"),
+    filter = cms.bool(True), # otherwise it won't filter the events
+)
+#BeamScrapingFilter
+process.NoScraping = cms.EDFilter("FilterOutScraping",
+ applyfilter = cms.untracked.bool(True),
+ debugOn = cms.untracked.bool(False),
+ numtrack = cms.untracked.uint32(10),
+ thresh = cms.untracked.double(0.25)
+)
+#phfCoincFilter2
+process.load("HeavyIonsAnalysis.Configuration.hfCoincFilter_cff")
+process.hfPosFilter2 = process.hfPosFilter.clone(minNumber=cms.uint32(2))
+process.hfNegFilter2 = process.hfNegFilter.clone(minNumber=cms.uint32(2))
+process.hfCoincFilter2 = cms.Sequence(
+    process.towersAboveThreshold 
+    + process.hfPosTowers 
+    + process.hfNegTowers 
+    + process.hfPosFilter2 
+    + process.hfNegFilter2
+)
+#all the event selections
+process.eventSelections = cms.Sequence(
+        process.primaryVertexFilter
+        + process.NoScraping
+        + process.hfCoincFilter2
+)
+
+#Centrality
+process.GlobalTag.snapshotTime = cms.string("9999-12-31 23:59:59.000")
+process.GlobalTag.toGet.extend([
+   cms.PSet(record = cms.string("HeavyIonRcd"),
+      tag = cms.string("CentralityTable_HFtowers200_DataXeXe_eff942_run2v9313x01_offline"),
+      connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+      label = cms.untracked.string("HFtowersCymbal5Ev8")
+   ),
+])
+process.load('RecoHI.HiCentralityAlgos.HiCentrality_cfi')
+process.hiCentrality.produceHFhits = False
+process.hiCentrality.produceHFtowers = True
+process.hiCentrality.produceEcalhits = False
+process.hiCentrality.produceZDChits = False
+process.hiCentrality.produceETmidRapidity = True
+process.hiCentrality.producePixelhits = False
+process.hiCentrality.produceTracks = True
+process.hiCentrality.producePixelTracks = False
+process.hiCentrality.reUseCentrality = False
+process.hiCentrality.srcTracks = cms.InputTag("generalTracks")
+process.hiCentrality.srcVertex = cms.InputTag("offlinePrimaryVertices")
+process.load("RecoHI.HiCentralityAlgos.CentralityBin_cfi")
+process.centralityBin.Centrality = cms.InputTag("hiCentrality")
+process.centralityBin.centralityVariable = cms.string("HFtowers")
+process.centralityBin.nonDefaultGlauberModel = cms.string("Cymbal5Ev8")
+
+#output
+process.TFileService = cms.Service("TFileService",
+                                  fileName=cms.string("flowXeXe_setupTest.root")
+)
+
+process.demo = cms.EDAnalyzer('XeXeAnalysis',
+   CentralitySrc    = cms.InputTag("hiCentrality"),
+   CentralityBinSrc = cms.InputTag("centralityBin","HFtowers"),
+   srcTracks = cms.InputTag("generalTracks"),
+   srcVertex= cms.InputTag("offlinePrimaryVertices"),
+   srcTower= cms.InputTag("towerMaker"),
+   UseQuality = cms.bool(True),
+   TrackQuality = cms.string('highPurity'),
+   vertexZMaxCut = cms.double(15.0),
+   trackPtMinCut = cms.double(0.3),
+   trackPtMaxCut = cms.double(100.0),
+   trackEtaCut = cms.double(2.4),
+   ptErrCut = cms.double(0.1),
+   dzRelCut = cms.double(3.0),
+   dxyRelCut = cms.double(3.0)
+)
+
+process.p = cms.Path(process.hltHIL1MinimumBiasHF_OR_SinglePixelTrack * process.eventSelections * process.hiCentrality * process.centralityBin * process.demo)
+
---
+++ setUpEventSelections/DATA/test/crab/crab3_multicrab_cfg.py
@@ -0,0 +1,48 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.transferOutputs = True
+config.General.transferLogs = True
+config.JobType.allowUndistributedCMSSW = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'ConfFile_cfg.py'
+config.Data.inputDataset = '/HIMinimumBias1/XeXeRun2017-PromptReco-v1/AOD'
+config.Data.inputDBS = 'global'
+config.Data.lumiMask = 'Cert_304899-304906_XeXe_PromptReco_Collisions17_JSON.txt'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 2
+config.Data.outLFNDirBase = '/store/user/tuos/XeXe/Flow/TestCentralityRun/v01Test_Cent_oct20'
+config.Data.publication = False
+config.Data.outputDatasetTag = 'xexe2017_flowcorr_HIMinimumBias1_v01Test_Cent_oct20'
+
+config.Site.storageSite = 'T2_US_Vanderbilt'
+
+if __name__ == '__main__':
+   from CRABAPI.RawCommand import crabCommand
+   from CRABClient.ClientExceptions import ClientException
+   from httplib import HTTPException
+
+   config.General.workArea = 'xexe2017_flowcorr_HIMinimumBias1_v01Test_Cent_oct20'
+
+   def submit(config):
+      try:
+           crabCommand('submit', config = config)
+      except HTTPException as hte:
+           print "Failed submitting task: %s" % (hte.headers)
+      except ClientException as cle:
+          print "Failed submitting task: %s" % (cle)
+   
+   for num in range(0,20):
+
+       print "Submitting Data Set %d " % (num+1)
+
+       RequestName = 'xexe2017_flowcorr_HIMinimumBias%d_v04Ana_5TevCentNoRecPt_oct16' % (num+1)
+       DataSetName = '/HIMinimumBias%d/HIRun2015-25Aug2016-v1/AOD' % (num+1)
+           
+       config.General.requestName = RequestName
+       config.Data.inputDataset = DataSetName
+       submit(config)
+
+# python crab3_multicrab_cfg.py to execute 
+# ./multicrab -c status -w crab_projects/ to check status 
